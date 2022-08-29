3199993f5a6e5094d319c47434e46cf6536bb7c3
tNhanDerivative <tnhanderivative@zohomail.com>
2022-08-30 05:28:53 +0700

    First working version

---
+++ nhan_TheNoobMaster/nhan_TheNoobMaster/settings/prod.py
@@ -0,0 +1,4 @@
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'os.getenv('DOMAIN_NAME')']
---
+++ nhan_TheNoobMaster/nhan_TheNoobMaster/urls.py
@@ -0,0 +1,62 @@
+"""project_name URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from posts.views import (
+    index,
+    search,
+    post_list,
+    post_detail,
+    post_create,
+    post_update,
+    post_delete,
+    IndexView,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+from marketing.views import email_list_signup
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', index),
+    path('', IndexView.as_view(), name='home'),
+    # path('blog/', post_list, name='post-list'),
+    path('blog/', PostListView.as_view(), name='post-list'),
+    path('search/', search, name='search'),
+    path('email-signup/', email_list_signup, name='email-list-signup'),
+    # path('create/', post_create, name='post-create'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
+    # path('post/<id>/', post_detail, name='post-detail'),
+    path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
+    # path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    # path('post/<id>/delete/', post_delete, name='post-delete'),
+    path('post/<pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('tinymce/', include('tinymce.urls')),
+    path('accounts/', include('allauth.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
