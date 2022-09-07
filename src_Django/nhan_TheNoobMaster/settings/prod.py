92c69a8a556da9208278a2f13d8e3da4ca7a7bb9
tnhanderivative <tnhanderivative@zohomail.com>
2022-09-07 10:29:36 +0000

    updating production settings

--- src_Django/nhan_TheNoobMaster/settings/prod.py
+++ src_Django/nhan_TheNoobMaster/settings/prod.py
@@ -1,7 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-
-DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'os.getenv('DOMAIN_NAME')']
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1',os.getenv('SERVER_IP'),  os.getenv('DOMAIN_NAME')]
 
 
 
